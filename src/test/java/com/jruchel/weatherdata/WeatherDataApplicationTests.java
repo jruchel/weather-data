79071499d0534de26860d6f463a435399d5005a9
jruchel <jruchel254@gmail.com>
2023-03-09 11:46:57 +0100

    Project setup

---
+++ src/test/java/com/jruchel/weatherdata/WeatherDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jruchel.weatherdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
